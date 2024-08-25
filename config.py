f4bec9e0c6ffb0d0918009266eb737a48d994d38
bala1802 <bala1802@live.com>
2024-08-25 21:40:02 +0530

    included Groq and Tavily API Keys

--- config.py
+++ config.py
@@ -9,4 +9,10 @@ LLM_MAX_TOKENS = 1000
 """
 Tavily Parameters
 """
+TAVILY_API_KEY = "<TAVILY_API_KEY>"
 NUMBER_OF_WEB_SEARCH_RESULTS = 3
+
+"""
+Groq Parameters
+"""
+GROQ_API_KEY = "<GROQ_API_KEY>"
