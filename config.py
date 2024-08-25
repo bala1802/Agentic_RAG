f755c0a5d2d652fe3262df37bccf56b909defcff
bala1802 <bala1802@live.com>
2024-08-25 18:58:39 +0530

    added tavily configurations

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+"""
+LLM Parameters
+"""
 OPENAI_API_BASE = "https://api.groq.com/openai/v1"
 MODEL_NAME = "llama3-8b-8192"
 LLM_TEMPERATURE = 0.1
 LLM_MAX_TOKENS = 1000
+
+"""
+Tavily Parameters
+"""
+NUMBER_OF_WEB_SEARCH_RESULTS = 3
